98fb927446aae338925fa0d2471bd3ca6a00b1f5
Indra Maulana <alkhusnaindra1@gmail.com>
2025-02-05 20:27:50 +0700

    update favicon and pwa features

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // Auto update service worker
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "InChat App",
+        short_name: "InChat",
+        description: "A simple PWA built with Vite and React",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     outDir: "dist",
   },
